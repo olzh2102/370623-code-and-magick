574cc8909b2769ecdb3328fd9481aad06d2daa41
olzhas2102 <olzhas2102@yahoo.com>
2017-11-23 14:19:40 +0600

    Добавил файли в index.html и исправил ошибки по times

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.cloudOfResutls =function (ctx, x, y, width, height) {
+window.cloudOfResutls = function (ctx, x, y, width, height) {
   var offset = 10;
   ctx.beginPath();
   ctx.moveTo(x, y);
@@ -24,7 +24,7 @@ window.renderStatistics = function (ctx, names, times) {
 
     for (var i = 0; i < array.length; i++) {
       var time = array[i];
-      if ( time > max) {
+      if (time > max) {
         max = time;
       }
     }
@@ -33,7 +33,7 @@ window.renderStatistics = function (ctx, names, times) {
   // ------- Results Shadow -------
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   window.cloudOfResutls(ctx, 120, 20, 420, 270);
-  //-----
+  // -----
 
   ctx.fillStyle = 'white';
   window.cloudOfResutls(ctx, 110, 10, 420, 270);
@@ -46,9 +46,8 @@ window.renderStatistics = function (ctx, names, times) {
 
   var histogramHeight = 150;
   var histogramWidth = 40;
-  var indent = 50 + histogramWidth;
+  var indent = 50;
   var initialX = 150;
-  var initialY = 100;
   var step = histogramHeight / (getMaxElementOfArray(times) - 0);
 
   var randomizeBlueColor = function () {
@@ -57,17 +56,17 @@ window.renderStatistics = function (ctx, names, times) {
 
   for (var i = 0; i < times.length; i++) {
     var name = names[i];
-    var height = step * (time - 0);
+    var height = step * (times[i] - 0);
 
     ctx.fillText(parseInt(times[i], 10), initialX + (histogramWidth + indent) * i, 90 + histogramHeight - height);
-    ctx.fillText(name, initialX + (histogramWidth + indent) * i, 100 + histogramHeigh + 20);
+    ctx.fillText(name, initialX + (histogramWidth + indent) * i, 100 + histogramHeight + 20);
     ctx.fillRect(initialX + (histogramWidth + indent) * i, 100 + histogramHeight - height, histogramWidth, height);
     ctx.fillStyle = '#000';
 
     if (name === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = getRandomBlueColor();
+      ctx.fillStyle = randomizeBlueColor();
     }
   }
 };
